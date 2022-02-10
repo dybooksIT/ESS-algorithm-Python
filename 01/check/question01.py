5012989de53ea5041561bfcca37e05fed3680b12
Dongyang IT <dybooks.it@gmail.com>
2022-02-10 13:15:05 +0900

    edit ch01 files #2

---
+++ 01/check/question01.py
@@ -0,0 +1,8 @@
+x = 3
+def calc(x):
+    x += 4
+    return x
+
+print(x)
+print(calc(x))
+print(x)
---
+++ 01/check/question02.py
@@ -0,0 +1,8 @@
+a = [3]
+def calc(a):
+    a[0] += 4
+    return a
+
+print(a)
+print(calc(a))
+print(a)
