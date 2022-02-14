bf6ba7b59d28a907f92f067fc4d397c109363b29
Dongyang IT <dybooks.it@gmail.com>
2022-02-14 16:57:11 +0900

    edit ch04 files #1

--- 04/binary_search.py
+++ 04/binary_search.py
@@ -4,13 +4,13 @@ def binary_search(data, value):
     while left <= right:
         mid = (left + right) // 2
         if data[mid] == value:
-            # 中央の値と一致した場合は位置を返す
+            # 중앙값과 일치하면 위치를 반환
             return mid
         elif data[mid] < value:
-            # 中央の値より大きい場合は探索範囲の左を変える
+            # 중앙값보다 크면 검색 범위의 왼쪽을 바꿈
             left = mid + 1
         else:
-            # 中央の値より小さい場合は探索範囲の右を変える
+            # 중앙값보다 작으면 검색 범위의 오른쪽을 바꿈
             right = mid - 1
     return -1
 
